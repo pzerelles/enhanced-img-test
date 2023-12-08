6fbd606c87383e4f495b98f05603a11b0694909e
Philipp Zerelles <philipp@zerelles.com>
2023-12-08 16:13:25 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<enhanced:img src="./example.jpg" alt="example" style="width: 640px; height: auto;" />
+<enhanced:img src="./example with spaces.jpg" alt="example" style="width: 640px; height: auto;" />
